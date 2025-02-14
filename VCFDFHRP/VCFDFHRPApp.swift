e43f808943d34426270c7b6774cfb31f73169dc6
Joe Chavez <joemchavez@gmail.com>
2025-02-14 10:12:24 -0800

    Initial Commit

---
+++ VCFDFHRP/VCFDFHRPApp.swift
@@ -0,0 +1,17 @@
+//
+//  VCFDFHRPApp.swift
+//  VCFDFHRP
+//
+//  Created by Chavez, Joe on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct VCFDFHRPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
